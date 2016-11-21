bc0946c89b973f32c9e8df9a9874902afa5d6e04
Jamie Pilgrim <jamieleepilgrim@gmail.com>
2016-11-21 14:25:53 -0800

    normalize, format nav and start create form

--- assets/scripts.js
+++ assets/scripts.js
@@ -16,8 +16,15 @@ $(document).ready(function(){
         // console.log(trip.name);
         $("<li><a href='#' class='pet' data-id=" + trip.id + ">" + trip.name + "</a>" + "<p> Continent: " + trip.continent + "</p>" + "<p> Weeks:" + trip.weeks + "</p></li>").appendTo($("#trips-list"));
       }); // .each end
+      
     }); // .get end
-  }); //show-trip on click end
+  }); //show-trips on click end
+
+
+  ///////////////////////////////////////
+  ///////////// SHOW A TRIP /////////////
+  ///////////////////////////////////////
+
 
 
 
@@ -25,7 +32,38 @@ $(document).ready(function(){
   ///////////// CREATE NEW TRIP /////////
   ///////////////////////////////////////
 
+  $('#create-trip').on('click', function(e){
+    e.preventDefault;
+    $('#new-trip').show();
+  }); //create-trip on click end
 
 
+  // Add Form Submit Post Request - have it show a message.
 
-});
+
+  //  Hides Form
+  $('#new-trip p').on('click', function(){
+    $('#new-trip').hide();
+  });
+
+
+  ///////////////////////////////////////
+  ////////  "HOMEPAGE" REDIRECT  ///////
+  // clicking on header h1 brings    //
+  // original page state           //
+  //////////////////////////////////
+
+
+  $('header h1').on('click', function(e){
+    e.preventDefault;
+    $('#trips-list').empty();
+  });
+
+
+  /////////////////////////////
+  //////////// FIN. //////////
+  ///////////////////////////
+  ///////*cue applause*/////
+  /////////////////////////
+
+}); // $(document).ready end
